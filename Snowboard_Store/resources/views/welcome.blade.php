a9d7534aba96cb67cb7e032485c29b3b410103a7
DarioGalleni <dariogalleni@yahoo.it>
2024-02-22 17:40:39 +0100

    ciao

---
+++ Snowboard_Store/resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+<x-layout>
+<header class="container">
+  <div class="row">
+    <div class="col-12 col-md-6">
+<video src="video/header.mp4" autoplay ></video>
+    </div>
+    <div class="col-12 col-md-6">
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti saepe cumque provident optio cupiditate nam, et, architecto magnam, placeat dolores repudiandae sit illo. At dolor necessitatibus, tempora placeat aliquid culpa?</p>
+    </div>
+  </div>
+</header>
+
+</x-layout>
