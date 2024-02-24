272138b5dcce0b8013ded68555e833a00c5e55a5
DarioGalleni <dariogalleni@yahoo.it>
2024-02-24 11:27:17 +0100

    ok

--- Snowboard_Store_Laravel/resources/views/components/layout.blade.php
+++ Snowboard_Store_Laravel/resources/views/components/layout.blade.php
@@ -5,6 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <title>Document</title>
 </head>
---
+++ Snowboard_Store_Laravel/resources/views/welcome.blade.php
@@ -0,0 +1,16 @@
+<x-layout>
+
+  <header class="container-fluid">
+    <div class="row justify-content-center mt-3">
+      <div class="col-12 text-center">
+        <h1>Lorem ipsum dolor sit amet</h1>
+      </div>
+      <div class="col-12 d-flex justify-content-center my-3">
+        <video autoplay loop class="rounded p-1 bg-light shadow">
+          <source src="video/header.mp4">
+        </video>
+      </div>
+  </header>
+
+
+</x-layout>
