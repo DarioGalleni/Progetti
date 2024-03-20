1b286b947bbcf874d3d2e308613af5504ddc5917
Dario <dariogalleni@yahoo.it>
2024-03-20 16:30:36 +0100

    ciao

--- Snowboard_Store_Laravel_inLavorazione/resources/views/welcome.blade.php
+++ Snowboard_Store_Laravel_inLavorazione/resources/views/welcome.blade.php
@@ -13,7 +13,7 @@
   <div class="container-fluid my-5">
     <div class="row justify-content-between">
       <div data-aos="fade-left" data-aos-duration="3000" class="col-12 col-md-6 d-flex align-items-center">
-        <h2>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestiae, id iusto! Vero error, cumque aliquid laudantium possimus blanditiis earum similique pariatur animi iste impedit, ex quod repellat dolorum, ab nihil!</h2>
+        <h2>DARIO Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestiae, id iusto! Vero error, cumque aliquid laudantium possimus blanditiis earum similique pariatur animi iste impedit, ex quod repellat dolorum, ab nihil!</h2>
       </div>
       <div data-aos="fade-right" data-aos-duration="3000" class="col-12 col-md-6 d-flex align-items-center bg-primary p-1 shadow rounded">
         <img src="img/header.jpg" alt="" class="img-fluid">
