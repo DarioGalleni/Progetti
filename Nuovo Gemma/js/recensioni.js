d7d9ceb1b9a2394b6873ac9b4442f6fb4a514aa9
DarioGalleni <dariogalleni@yahoo.it>
2024-06-02 22:11:36 +0200

    ciao

--- Nuovo Gemma/js/recensioni.js
+++ Nuovo Gemma/js/recensioni.js
@@ -1,13 +1,29 @@
 let recensioni = [
     {"nome": "Francesca", "recensione": "Sono stata alla Gemma del Mare solamente 1 notte assieme alla famiglia perchè eravamo in visita a parenti. Staff gentile e accogliente, posto incantevole e a pochi passi dalla spiaggia. Spero di riuscire a tornarci per un soggiorno più lungo"},
 
-    {"nome": "mario", "recensione": "galleni"}
+    {"nome": "mario", "recensione": "galleni"},
+    {"nome": "mario", "recensione": "falleni"}
   ];
 
-let sliderItem = document.querySelector(".slider-item");
+let reviews = document.querySelector("#reviews");
 
 recensioni.forEach((element) => {
-    let p = document.createElement("p");
-    p.innerHTML = `Recensore: ${element.nome} Recensione: ${element.recensione}`;
-    swiperWrapper.appendChild(p);
+    let div = document.createElement("div");
+    div.classList.add("carousel-item", "active");
+    div.innerHTML = `<div style="border: 1px solid black;" class="container">
+    <div class="row">
+      <div class="col-6">
+        <h2 class="text-end">Recensore: ${element.nome}</h2>
+      </div>
+      <div class="col-6">
+        <p>site-logo</p>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-12">
+        <h2 class="text-center">Recensione: ${element.recensione}</h2>
+      </div>
+    </div>
+  </div>`;
+    reviews.appendChild(div);
 });
