6eb0d36010eb27bc7dc70c704942d9d7dda1af5f
DarioGalleni <dariogalleni@yahoo.it>
2024-06-03 09:23:44 +0200

    jfhy

--- Nuovo Gemma/js/recensioni.js
+++ Nuovo Gemma/js/recensioni.js
@@ -1,5 +1,5 @@
 let recensioni = [
-    {"nome": "Francesca", "recensione": "Sono stata alla Gemma del Mare solamente 1 notte assieme alla famiglia perchè eravamo in visita a parenti. Staff gentile e accogliente, posto incantevole e a pochi passi dalla spiaggia. Spero di riuscire a tornarci per un soggiorno più lungo"},
+    {"nome": "Francesca", "recensione": "Sono stata alla Gemma del Mare solamente 1 notte assieme alla famiglia perchè eravamo in visita a parenti. Staff gentile e accogliente, posto incantevole e a pochi passi dalla spiaggia. Spero di riuscire a tornarci per un soggiorno più lungo", "fonte": "images/tripadvisor.png"},
 
     {"nome": "mario", "recensione": "galleni"},
     {"nome": "mario", "recensione": "falleni"}
@@ -16,7 +16,7 @@ recensioni.forEach((element) => {
         <h2 class="text-end">Recensore: ${element.nome}</h2>
       </div>
       <div class="col-6">
-        <p>site-logo</p>
+      <img class="img-thumbnail" src="images/tripadvisor.png" alt="">
       </div>
     </div>
     <div class="row">
