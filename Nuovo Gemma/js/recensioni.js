0cb6887415aa7c0198cae22f3146375c6f31d2cb
DarioGemma <dariogalleni@yahoo.it>
2024-06-06 11:05:09 +0200

    ciao

--- Nuovo Gemma/js/recensioni.js
+++ Nuovo Gemma/js/recensioni.js
@@ -10,17 +10,17 @@ let reviews = document.querySelector("#reviews");
 recensioni.forEach((element) => {
     let div = document.createElement("div");
     div.classList.add("carousel-item", "active");
-    div.innerHTML = `<div style="border: 1px solid black;" class="container">
+    div.innerHTML = `<div class="container">
     <div class="row">
-      <div class="col-6">
-        <h2 class="text-end">Recensore: ${element.nome}</h2>
+      <div class="col-6 ">
+        <h2 class="text-center">Recensore: ${element.nome}</h2>
       </div>
-      <div class="col-6">
-      <img class="img-thumbnail" src="images/tripadvisor.png" alt="">
+      <div class="col-6 d-flex justify-content-center">
+        <img src="images/tripadvisor.png" height="50px" class="" alt="">
       </div>
     </div>
     <div class="row">
-      <div class="col-12">
+      <div class="col-12 mt-3">
         <h2 class="text-center">Recensione: ${element.recensione}</h2>
       </div>
     </div>
