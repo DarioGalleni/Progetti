9439262f249ceb79ab628f0e75f3fc3f362609a3
DarioGalleni <dariogalleni@yahoo.it>
2024-06-12 12:25:46 +0200

    ciao

--- Nuovo Gemma/js/recensioni.js
+++ Nuovo Gemma/js/recensioni.js
@@ -1,27 +1,27 @@
-let recensioni = [
-    {"nome": "Francesca", "recensione": "Sono stata alla Gemma del Mare solamente una notte assieme alla famiglia perchè eravamo in visita a parenti. Staff gentile e accogliente, posto incantevole e a pochi passi dalla spiaggia. Spero di riuscire a tornarci per un soggiorno più lungo", "fonte": "images/tripadvisor.png"},
-    {"nome": "Antonella", "recensione": "Bellissima esperienza !!! Hotel da consigliare!!! Posizione perfetta nel centro di marina di Pietrasanta... Personale disponibile camere pulite e comode... Torneremo sicuramente", "fonte":"images/google.png"},
-    {"nome": "mario", "recensione": "falleni"}
+// let recensioni = [
+//     {"nome": "Francesca", "recensione": "Sono stata alla Gemma del Mare solamente una notte assieme alla famiglia perchè eravamo in visita a parenti. Staff gentile e accogliente, posto incantevole e a pochi passi dalla spiaggia. Spero di riuscire a tornarci per un soggiorno più lungo", "fonte": "images/tripadvisor.png"},
+//     {"nome": "Antonella", "recensione": "Bellissima esperienza !!! Hotel da consigliare!!! Posizione perfetta nel centro di marina di Pietrasanta... Personale disponibile camere pulite e comode... Torneremo sicuramente", "fonte":"images/google.png"},
+//     {"nome": "mario", "recensione": "falleni"}
     
-  ];
+//   ];
 
-let reviews = document.querySelector("#reviews");
+// let reviews = document.querySelector(".swiper-wrapper");
 
-recensioni.forEach((element) => {
-    let div = document.createElement("div");
-    div.classList.add("carousel-item", "active");
-  div.innerHTML = `<div class="card border-white">
-  <div class="card-header d-flex row justify-content-between text-center">
-    <div class="col-6 my-auto">
-      <h5>${element.nome}</h5>
-    </div>
-    <div class="col-6">
-      <img src="${element.fonte}" alt="">
-    </div>
-  </div>
-  <div class="card-body">
-    <p class="card-text text-center">${element.recensione}</p>
-  </div>
-</div>`
-    reviews.appendChild(div);
-});
+// recensioni.forEach((element) => {
+//     let div = document.createElement("div");
+//     div.classList.add("swiper-slide");
+//   div.innerHTML = `<div class="card border-white">
+//   <div class="card-header d-flex row justify-content-between text-center">
+//     <div class="col-6 my-auto">
+//       <h5>${element.nome}</h5>
+//     </div>
+//     <div class="col-6">
+//       <img src="${element.fonte}" alt="">
+//     </div>
+//   </div>
+//   <div class="card-body">
+//     <p class="card-text text-center">${element.recensione}</p>
+//   </div>
+// </div>`
+//     reviews.appendChild(div);
+// });
