1aa81d25bb38d7cb6868d830d43d59bd53515aec
DarioGalleni <dariogalleni@yahoo.it>
2024-06-16 22:24:43 +0200

    ok

--- Nuovo Gemma/js/recensioni.js
+++ Nuovo Gemma/js/recensioni.js
@@ -1,6 +1,7 @@
 let recensioni = [
     {"nome": "Francesca", "recensione": "Sono stata alla Gemma del Mare solamente una notte assieme alla famiglia perchè eravamo in visita a parenti. Staff gentile e accogliente, posto incantevole e a pochi passi dalla spiaggia. Spero di riuscire a tornarci per un soggiorno più lungo", "fonte": "images/tripadvisor.png"},
-    {"nome": "Antonella", "recensione": "Bellissima esperienza !!! Hotel da consigliare!!! Posizione perfetta nel centro di marina di Pietrasanta... Personale disponibile camere pulite e comode... Torneremo sicuramente", "fonte":"images/google.png"},    
+    {"nome": "Antonella", "recensione": "Bellissima esperienza !!! Hotel da consigliare!!! Posizione perfetta nel centro di marina di Pietrasanta... Personale disponibile camere pulite e comode... Torneremo sicuramente", "fonte":"images/google.png"},
+    {"nome": "Sabrina", "recensione": "Bellissimo hotel a gestione familiare, dove ci si sente praticamente a casa. Ottima cucina, distanza dal mare circa 1 km. Possibilità di affittare biciclette. Parcheggio interno gratuito", "fonte":"images/google.png"},   
   ];
 
 let reviews = document.querySelector(".swiper-wrapper");
