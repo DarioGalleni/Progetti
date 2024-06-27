8206ad97659d85b45808578c48c16a40a9d19caa
DarioGalleni <dariogalleni@yahoo.it>
2024-06-27 15:35:00 +0200

    ok

--- Nuovo Gemma/js/recensioni.js
+++ Nuovo Gemma/js/recensioni.js
@@ -1,26 +1,17 @@
 let recensioni = [
-    {"nome": "Francesca", "recensione": "Sono stata alla Gemma del Mare solamente una notte assieme alla famiglia perchè eravamo in visita a parenti. Staff gentile e accogliente, posto incantevole e a pochi passi dalla spiaggia. Spero di riuscire a tornarci per un soggiorno più lungo", "fonte": "images/tripadvisor.png"},
-    {"nome": "Antonella", "recensione": "Bellissima esperienza !!! Hotel da consigliare!!! Posizione perfetta nel centro di marina di Pietrasanta... Personale disponibile camere pulite e comode... Torneremo sicuramente", "fonte":"images/google.png"},
-    {"nome": "Sabrina", "recensione": "Bellissimo hotel a gestione familiare, dove ci si sente praticamente a casa. Ottima cucina, distanza dal mare circa 1 km. Possibilità di affittare biciclette. Parcheggio interno gratuito", "fonte":"images/google.png"},   
+    {"nome": "Francesca", "recensione": "Sono stata alla Gemma del Mare solamente una notte assieme alla famiglia perchè eravamo in visita a parenti. Staff gentile e accogliente, posto incantevole e a pochi passi dalla spiaggia. Spero di riuscire a tornarci per un soggiorno più lungo"},
+    {"nome": "Antonella", "recensione": "Bellissima esperienza !!! Hotel da consigliare!!! Posizione perfetta nel centro di marina di Pietrasanta... Personale disponibile camere pulite e comode... Torneremo sicuramente"},
+    {"nome": "Sabrina", "recensione": "Bellissimo hotel a gestione familiare, dove ci si sente praticamente a casa. Ottima cucina, distanza dal mare circa 1 km. Possibilità di affittare biciclette. Parcheggio interno gratuito"},   
   ];
 
-let reviews = document.querySelector(".swiper-wrapper");
+let reviews = document.querySelector(".js-carousel-2");
 
 recensioni.forEach((element) => {
     let div = document.createElement("div");
-    div.classList.add("swiper-slide");
-  div.innerHTML = `<div class="card border-white">
-  <div class="card-header d-flex row justify-content-between text-center">
-    <div class="col-6 my-auto">
-      <h5>${element.nome}</h5>
-    </div>
-    <div class="col-6">
-      <img src="${element.fonte}" alt="">
-    </div>
-  </div>
-  <div class="card-body">
-    <p class="card-text text-center">${element.recensione}</p>
-  </div>
-</div>`
+    div.style.minHeight = "300px"
+    div.innerHTML = `<p class="fst-italic">${element.recensione}}</p>
+            <div class="d-flex justify-content-center align-items-end">
+              <p class="fst-italic">${element.nome}</p>
+            </div>`
     reviews.appendChild(div);
 });
