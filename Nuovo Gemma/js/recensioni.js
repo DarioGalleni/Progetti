f7555bb3716e4f573b94540fbef5dfc17034129e
DarioGemma <dariogalleni@yahoo.it>
2024-07-04 17:22:23 +0200

    ok

--- Nuovo Gemma/js/recensioni.js
+++ Nuovo Gemma/js/recensioni.js
@@ -1,7 +1,9 @@
 let recensioni = [
-    {"nome": "Francesca", "recensione": "Sono stata alla Gemma del Mare solamente una notte assieme alla famiglia perchè eravamo in visita a parenti. Staff gentile e accogliente, posto incantevole e a pochi passi dalla spiaggia. Spero di riuscire a tornarci per un soggiorno più lungo"},
+    {"nome": "Francesca", "recensione": "Sono stata alla Gemma del Mare solamente una notte assieme alla famiglia perch&egrave eravamo in visita a parenti. Staff gentile e accogliente, posto incantevole e a pochi passi dalla spiaggia. Spero di riuscire a tornarci per un soggiorno pi&ugrave lungo"},
     {"nome": "Antonella", "recensione": "Bellissima esperienza !!! Hotel da consigliare!!! Posizione perfetta nel centro di marina di Pietrasanta... Personale disponibile camere pulite e comode... Torneremo sicuramente"},
-    {"nome": "Sabrina", "recensione": "Bellissimo hotel a gestione familiare, dove ci si sente praticamente a casa. Ottima cucina, distanza dal mare circa 1 km. Possibilità di affittare biciclette. Parcheggio interno gratuito"},   
+    {"nome": "Sabrina", "recensione": "Bellissimo hotel a gestione familiare, dove ci si sente praticamente a casa. Ottima cucina, distanza dal mare circa 1 km. Possibilit&agrave di affittare biciclette. Parcheggio interno gratuito"},
+    {"nome": "Martina", "recensione": "Purtroppo siamo rimasti solo una notte, il posto stupendo e a un passo dal mare. Gianni e Dario sono davvero accoglienti, professionali e super simpatici! Benessere e risate assicurate . Consigliatissimo, torneremo sicuramente in futuro !"},
+    {"nome": "Francesco", "recensione": "Il personale &egrave molto cordiale e molto simpatico e l'hotel &egrave molto bello,lo consiglierei vivamente"},   
   ];
 
 let reviews = document.querySelector(".js-carousel-2");
