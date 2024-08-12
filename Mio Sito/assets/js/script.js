0e2c035f78ebe263b3c1461451234b93d0e37278
DarioGalleni <dariogalleni@yahoo.it>
2024-08-12 16:32:45 +0200

    ok

--- Mio Sito/assets/js/script.js
+++ Mio Sito/assets/js/script.js
@@ -52,8 +52,24 @@ window.addEventListener('scroll', function() {
     }
 });
 
-// let submit = document.getElementById("submit")
-// submit.addEventListener("click", function()
-// {
-//     alert("ok")
-// })
+
+
+//whatsapp
+let submitButton = document.getElementById("whatsapp_submit");
+
+submitButton.addEventListener("click", function(event) {
+
+    event.preventDefault();
+    
+    let message = document.getElementById("whatsapp_message").value;
+    let name = document.getElementById("whatsapp_name").value;
+
+    let fullMessage = `Nome: ${name}%0A%0AMessaggio: ${message}`;
+    let whatsappURL = `https://wa.me/393298047791?text=${fullMessage}`;
+
+    window.open(whatsappURL, '_blank');
+
+    document.getElementById("whatsapp_message").value = "";
+    document.getElementById("whatsapp_name").value = "";
+});
+
